e470dd27356a506d96a7889056c0e7a57aa34549
Jared Dunn <jdunn@webucator.com>
2020-08-18 11:23:52 -0400

    slugs

--- jokes/admin.py
+++ jokes/admin.py
@@ -5,10 +5,10 @@ from .models import Joke
 @admin.register(Joke)
 class JokeAdmin(admin.ModelAdmin):
     model = Joke
-    list_display = ['question', 'created', 'updated']
+    list_display = ['question', 'created', 'updated', 'slug']
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
-            return ('created', 'updated')
+            return ('slug', 'created', 'updated')
 
         return ()
--- jokes/models.py
+++ jokes/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.urls import reverse
 
+from common.utils.text import unique_slug
+
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=200, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=50, unique=True, null=False, editable=False)
 
     def get_absolute_url(self):
         return reverse('jokes:detail', args=[str(self.pk)])
@@ -13,4 +16,9 @@ class Joke(models.Model):
     def __str__(self):
         return self.question
     
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
 
+        super().save(*args, **kwargs)
