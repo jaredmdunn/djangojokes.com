8060cb0fec10c1faab4b721745604e20eff91452
Jared Dunn <jdunn@webucator.com>
2020-08-20 16:57:25 -0400

    updates to joke add/update form

--- jokes/views.py
+++ jokes/views.py
@@ -4,11 +4,13 @@ from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
 
+from .forms import JokeForm
+
 from .models import Joke
 
 class JokeCreateView(CreateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
 
 class JokeDeleteView(DeleteView):
     model = Joke
@@ -22,4 +24,4 @@ class JokeListView(ListView):
 
 class JokeUpdateView(UpdateView):
     model = Joke
-    fields = ['question', 'answer']
+    form_class = JokeForm
