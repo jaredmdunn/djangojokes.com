ce03db6efb33311363c7bc0e69af76e0bb55bcf7
Jared Dunn <jdunn@webucator.com>
2020-09-03 13:28:58 -0400

    admin updates

--- jokes/models.py
+++ jokes/models.py
@@ -24,16 +24,38 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+class Tag(models.Model):
+    tag = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True, null=False, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('jokes:tag', args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.tag
+
+    class Meta:
+        ordering = ['tag']
+
 class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=200, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=50, unique=True, null=False, editable=False)
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def get_absolute_url(self):
-        return reverse('jokes:detail', args=[str(self.pk)])
+        return reverse('jokes:detail', args=[self.slug])
 
     def __str__(self):
         return self.question
