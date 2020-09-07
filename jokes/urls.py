50aef5babaa4a7795893c5303cf4f37da678e005
Jared Dunn <jdunn@webucator.com>
2020-09-07 17:58:19 -0400

    filtering

--- jokes/urls.py
+++ jokes/urls.py
@@ -7,10 +7,13 @@ from .views import (
 
 app_name = 'jokes'
 urlpatterns = [
+    path('category/<slug>/', JokeListView.as_view(), name='category'),
+    path('creator/<username>/', JokeListView.as_view(), name='creator'),
     path('joke/create/', JokeCreateView.as_view(), name='create'),
     path('joke/<slug>/delete/', JokeDeleteView.as_view(), name='delete'),
     path('joke/<slug>/', JokeDetailView.as_view(), name='detail'),
     path('joke/<slug>/vote/', vote, name='ajax-vote'),
     path('', JokeListView.as_view(), name='list'),
     path('joke/<slug>/update/', JokeUpdateView.as_view(), name='update'),
+    path('tag/<slug>/', JokeListView.as_view(), name='tag'),
 ]
