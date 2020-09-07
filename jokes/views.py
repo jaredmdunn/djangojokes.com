f35d5ac5779803c4ca72d6476fb4709acdc82cd7
Jared Dunn <jdunn@webucator.com>
2020-09-07 14:52:11 -0400

    json ajax for jokes

--- jokes/views.py
+++ jokes/views.py
@@ -1,8 +1,9 @@
-import datetime
+import json
 
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.http import JsonResponse
 from django.urls import reverse_lazy
 
 from django.views.generic import (
@@ -11,7 +12,7 @@ from django.views.generic import (
 
 from .forms import JokeForm
 
-from .models import Joke
+from .models import Joke, JokeVote
 
 class JokeCreateView(SuccessMessageMixin, CreateView):
     model = Joke
@@ -49,3 +50,53 @@ class JokeUpdateView(SuccessMessageMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         obj = self.get_object()
         return self.request.user == obj.user
+
+    def vote(request, slug):
+        user = request.user
+        joke = Joke.objects.get(slug=slug)
+        data = json.loads(request.body)
+
+        vote = data['vote']
+        likes = data['likes']
+        dislikes = data['dislikes']
+
+        if user.is_anonymous:
+            msg = 'Sorry, you have to be logged in to vote.'
+        else:
+            if JokeVote.objects.filter(user=user, joke=joke).exists():
+                joke_vote = JokeVote.objects.get(user=user, joke=joke)
+
+                if joke_vote.vote == vote:
+                    msg = 'Right. You told us already. Geez.'
+                else: 
+                    joke_vote.vote = vote
+                    joke_vote.save()
+
+                    if vote == -1:
+                        likes -= 1
+                        dislikes += 1
+                        msg = "Don't like it after all, huh? Ok. Noted."
+                    else:
+                        likes += 1
+                        dislikes -= 1
+                        msg = 'Grown on you, has it? Ok. Noted.'
+
+            else:
+                joke_vote = JokeVote(user=user, joke=joke, vote=vote)
+                joke_vote.save()
+
+                if vote == -1:
+                    likes -= 1
+                    dislikes += 1
+                    msg = "Sorry you didn't like the joke."
+                else:
+                    likes += 1
+                    dislikes -= 1
+                    msg = 'Yeah, good one, right?'
+
+        response = {
+            'msg' : msg,
+            'likes' : likes,
+            'dislikes' : dislikes
+        }
+        return JsonResponse(response)
